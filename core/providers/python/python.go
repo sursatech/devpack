4acaaed0e765e80a5d2ebc6c9003f9a13ec9b2d1
Jake Runzer <jakerunzer@gmail.com>
2025-09-04 22:44:10 -0400

    Revert "fix: use default mise backends for python packages (#249)" (#252)
    This reverts commit 36be38852403ec719848ebb5d1b98a08af027c57.

--- core/providers/python/python.go
+++ core/providers/python/python.go
@@ -311,25 +311,23 @@ func (p *PythonProvider) InstallMisePackages(ctx *generate.GenerateContext, mise
 		miseStep.Version(python, pipfileVersion, fmt.Sprintf("Pipfile > %s", pipfileVarName))
 	}
 
-	// TODO we should parse the python-version in pyproject.toml
+	if p.hasPoetry(ctx) || p.hasUv(ctx) || p.hasPdm(ctx) || p.hasPipfile(ctx) {
+		miseStep.Default("pipx", "latest")
+	}
 
 	if p.hasPoetry(ctx) {
-		miseStep.Default("poetry", "latest")
+		miseStep.Default("pipx:poetry", "latest")
 	}
 
 	if p.hasPdm(ctx) {
-		// the default pdm backend assumes that pipx is installed
-		miseStep.Default("pipx", "latest")
-		miseStep.Default("pdm", "latest")
+		miseStep.Default("pipx:pdm", "latest")
 	}
 
 	if p.hasUv(ctx) {
-		miseStep.Default("uv", "latest")
+		miseStep.Default("pipx:uv", "latest")
 	}
 
 	if p.hasPipfile(ctx) {
-		// default pipenv backend does not work with the default python backend
-		miseStep.Default("pipx", "latest")
 		miseStep.Default("pipx:pipenv", "latest")
 	}
 
@@ -436,7 +434,7 @@ func (p *PythonProvider) addMetadata(ctx *generate.GenerateContext) {
 func (p *PythonProvider) usesDep(ctx *generate.GenerateContext, dep string) bool {
 	for _, file := range []string{"requirements.txt", "pyproject.toml", "Pipfile"} {
 		if contents, err := ctx.App.ReadFile(file); err == nil {
-			// TODO: Do something better than string comparison, a comment could indicate a dep is included
+			// TODO: Do something better than string comparison
 			if strings.Contains(strings.ToLower(contents), strings.ToLower(dep)) {
 				return true
 			}
